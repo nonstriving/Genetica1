b3f14b1e2f4af248890acf37419dd9d3f2113d0f
samara <samara@Samaras-MacBook-Pro.local>
2018-09-28 14:58:13 -0300

    bla

---
+++ Genetica1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+bool equiv(int size, char *str1, char *str2)
+{
+  
+}
+
+int main()
+{
+  int size;
+  char *str1;
+  char *str2;
+
+  scanf("%d", &size);
+  scanf("%s", str1);
+  scanf("%s", str2);
