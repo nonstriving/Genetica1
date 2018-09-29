eb72d90101bf4b7547b84c2ba9ea5ec0244e8956
samara <samara@Samaras-MacBook-Pro.local>
2018-09-29 12:54:56 -0300

    bla

--- Genetica1.c
+++ Genetica1.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 bool equiv(int size, char *str1, char *str2)
 {
   bool a;
   bool b;
+  char *stra;
+  char *strb;
 
   if(size==2)
   {
@@ -16,8 +19,34 @@ bool equiv(int size, char *str1, char *str2)
   }
   else
   {
+    int i;
     size=size/2;
-    a=
+
+    stra=(char*)malloc(size * sizeof(char));
+    strb=(char*)malloc(size * sizeof(char));
+
+    for(i=0;i<size;i++)
+    {
+      stra[i]=str1[i];
+    }
+    for(i=size;i<size*2;i++)
+    {
+      strb[i]=str1[i];
+    }
+    a=equiv(size,stra,strb);
+
+    for(i=0;i<size;i++)
+    {
+      stra[i]=str2[i];
+    }
+    for(i=size;i<size*2;i++)
+    {
+      strb[i]=str2[i];
+    }
+    b=equiv(size,stra,strb);
+
+    return(a&&b);
+  }
 }
 
 int main()
@@ -25,7 +54,18 @@ int main()
   int size;
   char *str1;
   char *str2;
+  bool eq;
 
   scanf("%d", &size);
   scanf("%s", str1);
   scanf("%s", str2);
+
+  eq=equiv(size,str1,str2);
+
+  if(eq==true)
+    printf("Equivalent\n");
+  else
+    printf("Not equivalent\n");
+
+  return 0;
+}
