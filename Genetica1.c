1bf21800f937fefb7dc45f923e3c69a1c9e3d0b7
samara <samara@Samaras-MacBook-Pro.local>
2018-09-29 13:53:41 -0300

    example test cases satisfied

--- Genetica1.c
+++ Genetica1.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+/*
+bool equiv(int size, char *str1, char *str2)
+{
+  int i;
+  size=size/2;
+  char *str1a;
+  char *str1b;
+  char *str2a;
+  char *str2b;
+
+  str1a=(char *)malloc(size * sizeof(char));
+  str1b=(char *)malloc(size * sizeof(char));
+  str2a=(char *)malloc(size * sizeof(char));
+  str2b=(char *)malloc(size * sizeof(char));
+
+  for(i=0;i<size;i++)
+  {
+    str1a[i]=str1[i];
+    str2a[i]=str2[i];
+  }
+
+  for(i=size;i<size*2;i++)
+  {
+    str1b[i-size]=str1[i];
+    str2b[i-size]=str2[i];
+  }
+
+  if(strcmp(str1a,str2a)==0&&strcmp(str1b,str2b)==0)
+    return true;
+  else
+    if(strcmp(str1a,str2b)==0&&strcmp(str1b,str2a)==0)
+      return true;
+    else
+      return false;
+}
+*/
 
 bool equiv(int size, char *str1, char *str2)
 {
-  bool a;
-  bool b;
-  char *stra;
-  char *strb;
-
   if(size==2)
   {
     if(str1[0]==str2[0] && str1[1]==str2[1])
@@ -23,30 +56,40 @@ bool equiv(int size, char *str1, char *str2)
     int i;
     size=size/2;
 
-    stra=(char*)malloc(size * sizeof(char));
-    strb=(char*)malloc(size * sizeof(char));
+    char *str1a=(char *)malloc(size * sizeof(char));
+    char *str1b=(char *)malloc(size * sizeof(char));
+    char *str2a=(char *)malloc(size * sizeof(char));
+    char *str2b=(char *)malloc(size * sizeof(char));
 
     for(i=0;i<size;i++)
     {
-      stra[i]=str1[i];
+      str1a[i]=str1[i];
     }
     for(i=size;i<size*2;i++)
     {
-      strb[i]=str1[i];
+      str1b[i-size]=str1[i];
     }
-    a=equiv(size,stra,strb);
+    printf("%s\n", str1a);
+    printf("%s\n", str1b);
 
     for(i=0;i<size;i++)
     {
-      stra[i]=str2[i];
+      str2a[i]=str2[i];
     }
     for(i=size;i<size*2;i++)
     {
-      strb[i]=str2[i];
+      str2b[i-size]=str2[i];
     }
-    b=equiv(size,stra,strb);
+    printf("%s\n", str2a);
+    printf("%s\n", str2b);
 
-    return(a&&b);
+    if(equiv(size,str1a,str2a)&&equiv(size,str1b,str2b))
+      return true;
+    else
+      if(equiv(size,str1a,str2b)&&equiv(size,str1b,str2a))
+        return true;
+      else
+        return false;
   }
 }
 
@@ -58,6 +101,10 @@ int main()
   bool eq;
 
   scanf("%d", &size);
+
+  str1=(char *)malloc(size * sizeof(char));
+  str2=(char *)malloc(size * sizeof(char));
+
   scanf("%s", str1);
   scanf("%s", str2);
 
