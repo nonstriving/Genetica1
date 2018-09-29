cf266a3b26802fa887911a5ed91046150dd34093
samara <samara@Samaras-MacBook-Pro.local>
2018-09-29 10:51:07 -0300

    bla

--- Genetica1.c
+++ Genetica1.c
@@ -3,7 +3,21 @@
 
 bool equiv(int size, char *str1, char *str2)
 {
-  
+  bool a;
+  bool b;
+
+  if(size==2)
+  {
+    if(str1[0]==str2[0] && str1[1]==str2[1])
+      if(str1[0]==str2[1] && str1[1]==str2[0])
+        return true;
+    else
+      return false;
+  }
+  else
+  {
+    size=size/2;
+    a=
 }
 
 int main()
